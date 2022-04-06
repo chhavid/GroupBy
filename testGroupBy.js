c1981dc8e12c685577da007b179ffb4d7b78f45a
Chhavi Dandriyal <99489321+chhavid@users.noreply.github.com>
2022-04-06 14:37:31 +0530

    Added test case for empty element.

--- testGroupBy.js
+++ testGroupBy.js
@@ -7,9 +7,10 @@ const testGroupElements = function (array, expGroups, description) {
 };
 
 const groupElementsTests = function () {
+  testGroupElements([], [], 'Empty element');
   testGroupElements([1], [[1]], 'Single element');
   testGroupElements([1, 3], [[1], [3]], 'Two different elements');
-  testGroupElements([1, 2, 1], [[1, 1], [2]], 'Two same and one differen element');
+  testGroupElements([1, 2, 1], [[1, 1], [2]], 'Two same and one different element');
   testGroupElements([1, 2, 3, 1, 2, 4], [[1, 1], [2, 2], [3], [4]], 'Multiple elements');
   testGroupElements([[1]], [[[1]]], 'Single element as arrays');
   testGroupElements([[1, 1], 1, [1, 1]], [[[1, 1], [1, 1]], [1]], 'With number and arrays');
